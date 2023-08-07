b47f5aeaf72438165557376cd76210b39d1db6da
HeynSekk <heynxsehhz@gmail.com>
2023-08-07 22:49:53 +0630

    make test failed

--- lib/main.dart
+++ lib/main.dart
@@ -97,7 +97,9 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () {
+          _incrementCounter();
+        },
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
