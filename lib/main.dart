8ca27506bc3b5a6f204103a34c36f8e47648a8cd
HeynSekk <heynxsehhz@gmail.com>
2023-08-08 09:52:15 +0630

    codes with bugs

--- lib/main.dart
+++ lib/main.dart
@@ -98,7 +98,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          _incrementCounter();
+          // _incrementCounter();
         },
         tooltip: 'Increment',
         child: const Icon(Icons.add),
