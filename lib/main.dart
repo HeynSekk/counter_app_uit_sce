d1919c2fcc0cce2a07e19f5a329e6e49d5daa4fe
HeynSekk <heynxsehhz@gmail.com>
2023-08-12 20:08:05 +0630

    add feature 2

--- lib/main.dart
+++ lib/main.dart
@@ -98,7 +98,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          //add a new feature
+          //add a new feature 2
           _incrementCounter();
         },
         tooltip: 'Increment',
